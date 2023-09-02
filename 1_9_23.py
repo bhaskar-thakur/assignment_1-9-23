8c958a737139da4ebd12760c38fd375085f6210d
bhaskar-thakur <143820431+bhaskar-thakur@users.noreply.github.com>
2023-09-02 14:01:35 +0530

    Add files via upload

---
+++ 1_9_23.py
@@ -0,0 +1,13 @@
+a=eval(input("enter the value of first number"))
+b=eval(input("enter the value of second number"))
+
+print(a//b)
+
+print(a**b)
+
+if a==b:
+    print("True")
+else:
+    print("False")
+
+
